9e8f49b6ab128ecc0e3aa19beae00d9f5dd8af33
Ask Hjorth Larsen <asklarsen@gmail.com>
2017-08-31 20:25:41 +0200

    work

--- presentation/mpi/mpihello2.c
+++ presentation/mpi/mpihello2.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     MPI_Status status;
     if(rank == 0) {
         num = 42.0;  // Pass number around to each process
-        printf("rank 0 sends %f to rank 1\n", mynumber);
+        printf("rank 0 sends %f to rank 1\n", num);
         MPI_Send(&num, 1, MPI_DOUBLE, 1, 0, comm);
         MPI_Recv(&num, 1, MPI_DOUBLE, size - 1, 0, comm, &status);
         printf("rank 0 finally received %f\n", num);
